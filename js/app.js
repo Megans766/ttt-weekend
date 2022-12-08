1b0f63b0e46dc922aa867621d5af60f201669537
Megan Smith <megans766@gmail.com>
2022-12-08 15:38:46 -0500

    completed numbers 1 & 2

--- js/app.js
+++ js/app.js
@@ -1,14 +1,18 @@
 /*-------------------------------- Constants --------------------------------*/
 
-
+const squareEls = document.getElementsByClassName("sqr")
+// console.log(squareEls);
+const messageEl = document.getElementById("message")
+// console.log(messageEl);
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-
+let board, turn, winner, tie
 
 /*------------------------ Cached Element References ------------------------*/
 
-
+document.getElementsByClassName("sqr")
+document.getElementById("message")
 
 /*----------------------------- Event Listeners -----------------------------*/
 
