8e4b00f2d566c013e8b3fd565005b9754a336bcd
Megan Smith <megans766@gmail.com>
2022-12-09 16:27:10 -0500

    complete steps 4 & 5

--- js/app.js
+++ js/app.js
@@ -1,34 +1,28 @@
 /*-------------------------------- Constants --------------------------------*/
 
-const squareEls = document.getElementsByClassName("sqr")
+const squareEls = document.querySelectorAll("div")
 // console.log(squareEls);
 const messageEl = document.getElementById("message")
 // console.log(messageEl);
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6]
+]
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-let board = {
-    sq0: null,
-    sq1: null,
-    sq2: null,
-    sq3: null,
-    sq4: null,
-    sq5: null,
-    sq6: null,
-    sq7: null,
-    sq8: null
-}
-// console.log(board);
- let turn = 1
-//  console.log(turn);
- let winner = false
-//  console.log(winner);
- let tie = false
-//  console.log(tie);
+let board, turn, winner,tie
+
 
 /*------------------------ Cached Element References ------------------------*/
 
-document.getElementsByClassName("sqr")
+document.querySelectorAll("div")
 document.getElementById("message")
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -38,7 +32,46 @@ document.getElementById("message")
 /*-------------------------------- Functions --------------------------------*/
 
 function init() {
-    // console.log(" the start");
+    board = [null, null, null, null, null, null, null, null, null]
+    turn = 1
+    winner = false
+    tie = false
     render()
 }
 init()
+
+function render() {
+    updateBoard()
+    updateMessage()
+}
+
+function updateBoard() {
+    board.forEach(function(squ, idx) {
+        if (squ === 1) {
+           return squareEls[idx].innerHTML = "X"
+        }else if(squ === -1) {
+            return squareEls[idx].innerHTML = "O"
+        } else {
+            return squareEls[idx].innerHTML = ""
+        }
+    })
+}
+
+function updateMessage() {
+    if (winner === false && tie === false) {
+        messageEl.textContent = `Player ${turn === 1? 'X' : 'O'} turn`
+    } else if (winner === false && tie === true) {
+        messageEl.textContent = "It's a tie!"
+    } else {
+        messageEl.textContent = `Player ${turn === -1? 'O' : 'X'} wins!`
+    }
+}
+
+function handleClick(evt) {
+
+}
+
+
+
+
+
